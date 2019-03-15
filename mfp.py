d02649bc13d16ce30d631c09ffbef8c9c90b7e30
Ajay <vaporsnek@gmail.com>
2019-03-15 16:53:50 +0530

    MFP Exercise Data

--- mfp.py
+++ mfp.py
@@ -1,36 +1,59 @@
-#Import Functions
+import re
 from bs4 import BeautifulSoup 
 from urllib.request import urlopen
 from urllib.request import Request
 
-#Search Parameters 
-#pageNo='1'
-#searchURL='dosa'
+# Search Parameters
+ 
+pageNo='1'
+searchURL='dosa'
 
-#Open and get link from search page
+def getPage(url):
+    ureq = Request(url, headers={'User-Agent':'Mozilla/5.0'})
+    return BeautifulSoup(urlopen(ureq).read(), 'lxml')
+
+
+# Search MFP
 url = 'https://www.myfitnesspal.com/food/search?page='+ pageNo + '&search='+ searchURL
-ureq = Request(url, headers={'User-Agent':'Mozilla/5.0'})
-htmlPage = urlopen(ureq)
-soup = BeautifulSoup(htmlPage.read(), 'lxml')
-items = soup.find("div", class_="food_info")
-url = 'https://www.myfitnesspal.com'+str(items.a["href"])
+searchSoup = getPage(url)
+item = searchSoup.find("div", class_="food_info")
 
-#Open 1st result page
-ureq = Request(url, headers={'User-Agent':'Mozilla/5.0'})
-htmlPage = urlopen(ureq)
-soup = BeautifulSoup(htmlPage.read(), 'lxml')
+# Get Results
+url = 'https://www.myfitnesspal.com'+str(item.a["href"])
+soup = getPage(url)
 
-# itemName = soup.find("div", class_="root-1W4Ez")
-# itemQuestion = soup.find("h1", class_="title-1P2uF")
-# print("????: "+itemQuestion.text)
-itemName = soup.find("h3", class_="jss3 jss17 pageTitle-_5t9X")
-itemCal = soup.find("h1", class_="title-cgZqW")
-itemCarb = soup.find("h1", class_="carbohydrates-orw9p subtitle-7_FIi")
-itemProtein = soup.find("h1",class_="protein-Eg08Q subtitle-7_FIi")
-itemFat = soup.find("h1",class_="fat-lHyji subtitle-7_FIi")
-print("Food Name: "+itemName.text)
-print("Calories: "+itemCal.text)
-print("Carbs: "+itemCarb.text)
-print("Protein: "+itemProtein.text)
-print("Fat: "+itemFat.text)
+#========================================================Parse Output=============================================
 
+# Food Info
+itemName = soup.find("h3", class_="jss3 jss17 pageTitle-_5t9X").text[itemName.text.find(' - ')+3:]
+itemCal = soup.find("h1", class_="title-cgZqW").text
+itemCarb = soup.find("h1", class_="carbohydrates-orw9p subtitle-7_FIi").text
+itemProtein = soup.find("h1",class_="protein-Eg08Q subtitle-7_FIi").text
+itemFat = soup.find("h1",class_="fat-lHyji subtitle-7_FIi").text
+
+print("Food Name: "+itemName)
+print("Calories: "+itemCal)
+print("Carbs: "+itemCarb)
+print("Protein: "+itemProtein)
+print("Fat: "+itemFat)
+
+# String Formatting for Exercise
+
+acString= soup.find("div",class_="inlineActivitiesContainer-2DZep").text
+repDict = {"Minutes": " Minutes", "Hours": " Hours","Seconds":" Seconds"} 
+pattern = re.compile("|".join(repDict.keys()))
+acString = pattern.sub(lambda m: repDict[m.group(0)], acString)
+
+cyindex = acString.find('of Cycling')
+ruindex = acString.find('of Running')
+clindex = acString.find('of Cleaning')
+
+# Exercise Info
+
+timeCyc=acString[:cyindex]
+timeRun=acString[cyindex+10:ruindex]
+timeClean=acString[ruindex+10:clindex]
+
+print("Cycling: " + timeCyc) 
+print("Running: " + timeRun) 
+print("Cleaning: " + timeClean) 
