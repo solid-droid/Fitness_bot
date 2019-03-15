23d6c1985ff3e8d595d4eb2f122774a33fd34188
Abhijith <abhijithka96@gmail.com>
2019-03-16 03:46:08 +0530

    fixed bugs

--- scrap.py
+++ scrap.py
@@ -143,10 +143,10 @@ def getItems(subsect):
         subset_dict[name]=cost
     return subset_dict    
         
-def getPrice(search_item, dish_name):
+def getPrice(search_item="mariot", dish_name="chickenbiryani"):
     menu=getMenu(search_item)
     all_items, _=getDict(menu)
-    print(all_items.keys())
+    print(all_items)
     dish_name=dish_name.replace(" ","").lower()
     try:
         return all_items[dish_name]
@@ -169,7 +169,14 @@ def getItemByName(menu, itemname):
         if(i.find(itemname)!=-1):
             itemlist.append((i, menu_dict[i]),)
     return itemlist       
-            
+def menuJson(search_item):
+    menu=getMenu(search_item)
+    menu=getDict(menu)
+    filename="menu_json"
+    filehandle=open(filename, "w")
+    filehandle.write(json.dumps(menu))
+    filehandle.close()
+               
             
             
 #menu=getMenu("thaal kitchen kakkanad")
@@ -182,8 +189,12 @@ def getItemByName(menu, itemname):
 #subs=menu["maincourse"]
 #sublist=getSubset(menu)
 #print(sublist)
-price=getPrice("hotel thaal edapally", "chicken65")
-print(price)
+#price=getPrice("iftar", "chickenbiriyani")
+#print(price)
+#menuJson("aavi")
+#price=getPrice("iftar", "chickenbiryani")
+#print(price)
+
 
     
     
